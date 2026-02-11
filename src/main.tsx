42258dbac20242f1aa5ec225d49a27d38cd441fa
Saqlain <riyanahmed1703@gmail.com>
2026-02-11 18:00:41 +0530

    Added temp routing

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,16 @@ import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 import "./index.css";
 
+// Redirect root domain to www subdomain
+if (typeof window !== "undefined" && window.location.hostname === "convertwise.site") {
+  window.location.replace(
+    "https://www.convertwise.site" +
+    window.location.pathname +
+    window.location.search +
+    window.location.hash
+  );
+}
+
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
     <BrowserRouter>
